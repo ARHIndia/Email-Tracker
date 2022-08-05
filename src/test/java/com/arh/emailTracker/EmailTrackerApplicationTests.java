124a92703308397e309eab0313c2e09d751e59da
Rohitanegi <arhtechfreelance@gmail.com>
2022-08-05 12:51:53 +0530

    sd

---
+++ src/test/java/com/arh/emailTracker/EmailTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.arh.emailTracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmailTrackerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
